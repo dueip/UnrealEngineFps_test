10be5183e9459153378dfbe745a9ba88ddd9bae7
dueip <68518940+dueip@users.noreply.github.com>
2024-04-15 03:45:15 +0500

    feat: Добавить компонент лазера в root directory

---
+++ Source/LestaStart/Core/LaserComponent.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LaserComponent.h"
+
+
+// Sets default values for this component's properties
+ULaserComponent::ULaserComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void ULaserComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void ULaserComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
