3d1ff0970724688b95372a50f5cbb5a24f818ded
dueip <68518940+dueip@users.noreply.github.com>
2024-04-15 08:00:41 +0500

    fix: Добавить комменатрий к использованию сокетов

--- Source/LestaStart/Core/LaserWeaponComponent.cpp
+++ Source/LestaStart/Core/LaserWeaponComponent.cpp
@@ -30,6 +30,8 @@ void ULaserWeaponComponent::TickComponent(float DeltaTime, ELevelTick TickType,
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 	Shoot();
+	// It's fine to use GetSocketLocation in here since it will return the component's transform anyways
+	// See: https://docs.unrealengine.com/4.27/en-US/API/Runtime/Engine/Components/USceneComponent/GetSocketLocation/
 	const FVector SocketOrigin = ThisClass::GetSocketLocation(GetAttachSocketName()); 
 	Laser->SetOrigin(SocketOrigin);
 	Laser->SetEndPoint(SocketOrigin + FVector(100, 0, 0));
