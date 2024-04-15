5e0ef97eae2bdc88634a45b7b61432d268caf2cb
dueip <68518940+dueip@users.noreply.github.com>
2024-04-16 02:23:50 +0500

    feat: Добавить стейты для турелек

--- Source/LestaStart/Core/Turret.cpp
+++ Source/LestaStart/Core/Turret.cpp
@@ -9,24 +9,55 @@ ATurret::ATurret()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	// Since the laser can also be given through the blueprints
-	if (!IsValid(LaserWeapon))
-	{
-		// Probably a bad choice?
-		
-	}
+	BeginningMode = Modes::None;
+	ViewRadius = 10.f;
+	FOV = 2 * UE_PI;
 }
 
 // Called when the game starts or when spawned
 void ATurret::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	// Set the mode to the current
+	CurrentMode = BeginningMode;
+	if (CurrentMode == Modes::None)
+	{
+		CurrentMode = Modes::Scouting;
+	}
+}
+
+void ATurret::ChangeStateTo(const Modes Mode)
+{
+	CurrentMode = Mode;
 }
 
 // Called every frame
 void ATurret::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	switch (CurrentMode)
+	{
+	case Modes::Scouting:
+		{
+			FRotator DeltaRotator = FRotator(0, UE_PI / 2, 0) * DeltaTime * 10.f; 
+			AddActorWorldRotation(DeltaRotator);
+			CurrentMode = Modes::Activated;
+		
+			break;
+		}
+	case Modes::Activated:
+		if (!TimerHandle.IsValid())
+		{
+			GetWorldTimerManager().SetTimer(TimerHandle, this, &ThisClass::	ChangeStateToAttack, 10.f);
+		}
+		break;
+	case Modes::Attacking:
+		{
+			FRotator DeltaRotator = FRotator(0, UE_PI / 2, 0) * DeltaTime * 100.f; 
+			AddActorWorldRotation(DeltaRotator);
+			//CurrentMode = Modes::Activated;
+		}
+	}
+		
 }
 
--- Source/LestaStart/Core/Turret.h
+++ Source/LestaStart/Core/Turret.h
@@ -7,12 +7,25 @@
 #include "LaserWeaponComponent.h"
 #include "Turret.generated.h"
 
+UENUM()
+enum class Modes
+{
+	None = 0	UMETA(Hidden), 
+	Alarm		UMETA(DisplayName="Alarm"),
+	Activated	UMETA(DisplayName="Activated"),
+	Attacking	UMETA(DisplayName="Attacking"),
+	Deactivated UMETA(DisplayName="Deactivated"),
+	Scouting	UMETA(DisplayName="Scouting")
+};
+
 UCLASS()
 class LESTASTART_API ATurret : public AActor
 {
 	GENERATED_BODY()
 
 public:
+	UPROPERTY(EditDefaultsOnly, Category="Mode")
+	Modes BeginningMode;	
 	// Sets default values for this actor's properties
 	ATurret();
 
@@ -21,9 +34,23 @@ protected:
 	virtual void BeginPlay() override;
 
 public:
-	UPROPERTY(EditAnywhere, Category="Weapon")
-	TObjectPtr<ULaserWeaponComponent> LaserWeapon;
+	UFUNCTION(BlueprintCallable)
+	void ChangeStateTo(const Modes Mode);
+	
+	UPROPERTY(EditAnywhere, Category="Vision")
+	float ViewRadius;
+
+	UPROPERTY(EditAnywhere, Category="Vision")
+	// Should somehow display that as degrees?
+	float FOV;
+	
 	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+private:
+	FTimerHandle TimerHandle;
+
+	void ChangeStateToAttack() { UE_LOG(LogTemp,Warning, TEXT("Changed to attack?")); ChangeStateTo(Modes::Attacking);} ;
+	Modes CurrentMode;
 };
