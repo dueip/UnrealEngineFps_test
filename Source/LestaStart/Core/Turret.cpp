21dbea366831ec8a5dd858a4e41f9d541b69acf2
dueip <68518940+dueip@users.noreply.github.com>
2024-04-17 02:09:49 +0500

    feat: Добавить отслеживание дистанции до игрока

--- Source/LestaStart/Core/Turret.cpp
+++ Source/LestaStart/Core/Turret.cpp
@@ -3,6 +3,8 @@
 
 #include "Turret.h"
 
+#include "Kismet/GameplayStatics.h"
+
 
 // Sets default values
 ATurret::ATurret()
@@ -10,7 +12,7 @@ ATurret::ATurret()
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	BeginningMode = Modes::None;
-	ViewRadius = 10.f;
+	ViewRadius = 500.f;
 	FOV = 2 * UE_PI;
 }
 
@@ -41,19 +43,32 @@ void ATurret::Tick(float DeltaTime)
 		{
 			FRotator DeltaRotator = FRotator(0, UE_PI / 2, 0) * DeltaTime * 10.f; 
 			AddActorWorldRotation(DeltaRotator);
-			CurrentMode = Modes::Activated;
-		
+			const APawn* Pawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+			if (Pawn)
+			{
+				FHitResult Hit;
+
+				const FVector TurretLocation = GetActorLocation();
+				const FVector TraceEnd = Pawn->GetActorLocation();
+				bool bBlockHit = GetWorld()->LineTraceSingleByChannel(Hit, TurretLocation, TraceEnd, ECC_Pawn);
+				const float DistanceFromTheTurret = FVector::Distance(TurretLocation, TraceEnd);
+				UE_LOG(LogTemp, Warning, TEXT("ViewRadius %f, Hit.Distance %f"), ViewRadius, DistanceFromTheTurret);	
+				if (Hit.bBlockingHit && DistanceFromTheTurret <= ViewRadius)
+				{
+					ChangeStateTo(Modes::Activated);
+				}
+			}
 			break;
 		}
 	case Modes::Activated:
 		if (!TimerHandle.IsValid())
 		{
-			GetWorldTimerManager().SetTimer(TimerHandle, this, &ThisClass::	ChangeStateToAttack, 10.f);
+			GetWorldTimerManager().SetTimer(TimerHandle, this, &ThisClass::	ChangeStateToAttack, 1.f);
 		}
 		break;
 	case Modes::Attacking:
 		{
-			FRotator DeltaRotator = FRotator(0, UE_PI / 2, 0) * DeltaTime * 100.f; 
+			FRotator DeltaRotator = FRotator(0, UE_PI / 2, 0) * DeltaTime * 500.f; 
 			AddActorWorldRotation(DeltaRotator);
 			//CurrentMode = Modes::Activated;
 		}
