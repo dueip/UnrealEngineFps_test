d631c6f9912b9cb42dcf3bff1a804c7dc4efb897
dueip <68518940+dueip@users.noreply.github.com>
2024-04-21 11:10:35 +0500

    feat: Добавить инвентарь и базовые функции

---
+++ Source/LestaStart/Core/WeaponInvenotryComponent.cpp
@@ -0,0 +1,51 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WeaponInvenotryComponent.h"
+
+
+// Sets default values for this component's properties
+UWeaponInvenotryComponent::UWeaponInvenotryComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	
+}
+
+
+// Called when the game starts
+void UWeaponInvenotryComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+IWeaponInterface* UWeaponInvenotryComponent::GetWeaponAt(const int32 Index)
+{
+	return Weapons[Index];
+}
+
+const IWeaponInterface* UWeaponInvenotryComponent::GetWeaponAt(const int32 Index) const
+{
+	return GetWeaponAt(Index);
+}
+
+void UWeaponInvenotryComponent::PushWeapon(IWeaponInterface* Weapon)
+{
+	Weapons.Push(Weapon);
+}
+
+
+// Called every frame
+void UWeaponInvenotryComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                              FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Source/LestaStart/Core/WeaponInvenotryComponent.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "WeaponInterface.h"
+#include "WeaponInvenotryComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class LESTASTART_API UWeaponInvenotryComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UWeaponInvenotryComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	IWeaponInterface* GetWeaponAt(const int32 Index);
+	
+	UFUNCTION(BlueprintCallable)
+	const IWeaponInterface* GetWeaponAt(const int32 Index) const; 
+	
+	UFUNCTION(BlueprintCallable)
+	void PushWeapon(IWeaponInterface* Weapon);
+	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	TArray<IWeaponInterface*> Weapons;
+};
